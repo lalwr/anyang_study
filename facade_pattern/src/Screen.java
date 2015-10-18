24fda7e2a8f723f98f2b2083d1cd5eb76d5e6ffc
fsali <fsali@naver.com>
2015-10-18 18:52:41 +0900

    facade patten 완성

---
+++ facade_pattern/src/Screen.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2015-10-18.
+ */
+public class Screen {
+    String description;
+
+    public Screen(String description) {
+        this.description = description;
+    }
+
+    public void up() {
+        System.out.println(description + " going up");
+    }
+
+    public void down() {
+        System.out.println(description + " going down");
+    }
+
+
+    public String toString() {
+        return description;
+    }
+}
---
+++ facade_pattern/src/TheaterLights.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2015-10-18.
+ */
+public class TheaterLights {
+    String description;
+
+    public TheaterLights(String description) {
+        this.description = description;
+    }
+
+    public void on() {
+        System.out.println(description + " on");
+    }
+
+    public void off() {
+        System.out.println(description + " off");
+    }
+
+    public void dim(int level) {
+        System.out.println(description + " dimming to " + level  + "%");
+    }
+
+    public String toString() {
+        return description;
+    }
+}
